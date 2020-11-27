a410b9770c5a105a4e0ba81967addeb06fb6cf28
tadeuasarro <tadeutas@gmail.com>
2020-11-27 18:12:00 -0300

    Add button to fire modal

--- src/tasks_listing.js
+++ src/tasks_listing.js
@@ -13,6 +13,8 @@ const tasksListing = (tasks, task_container, verify) => {
     const info = document.createElement('span')
     info.innerHTML= "Tast title: " + key[j].title + " Date due " + key[j].dueDate;
 
+    const btnContainer = document.createElement('div')
+
     const delButton = document.createElement('div');
     delButton.classList.add('btn', 'btn-danger');
     delButton.innerHTML = 'Delete';
@@ -27,8 +29,18 @@ const tasksListing = (tasks, task_container, verify) => {
       }
     })
 
+    const editButton = document.createElement('div');
+    editButton.classList.add('btn', 'btn-info', 'mr-2');
+    editButton.innerHTML = 'Edit';
+    editButton.dataset.target = '#myModal';
+    editButton.dataset.toggle = 'modal';
+
+    btnContainer.appendChild(editButton);
+
+    btnContainer.appendChild(delButton);
+
     dflex.appendChild(info);
-    dflex.appendChild(delButton);
+    dflex.appendChild(btnContainer);
 
     list_item.appendChild(dflex);
 
