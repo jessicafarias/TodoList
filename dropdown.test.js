88695fead495781924a74565bb3157e56c62b6c3
tadeuasarro <tadeutas@gmail.com>
2020-12-07 19:29:09 -0300

    Add first testing case

--- dropdown.test.js
+++ dropdown.test.js
@@ -1,28 +1,7 @@
 import dropdown from './src/dropdown';
 
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+document.body.innerHTML = '<select id="dropdown"></select>';
 
-beforeEach(function() {
-  return JSDOM.fromFile('./dist/index.html')
-  .then((dom) => {
-    console.log(dom.window.document.getElementById('create_task').textContent +"sjkdn");
-  });
-}); //Display the text inside create_task
-
-
-test('TESTING DROPDOWN', () => {
-  let a =true;
-  beforeEach(function() {
-    JSDOM.fromFile('./dist/index.html')
-    .then((dom) => {
-      //console.log(dom.window.document.getElementById('create_task').textContent);
-      a=false;//Do not oveerride
-      expect(dropdown()).toBeFalsy(); //No passe
-      //expect(dropdown()).toBetrue(); //Pass
-    });
-  });
-  expect(a.toBeFalsy)
-  console.log(a);
-}); //Do not override a;
-
+test('Testing Dropdown', () => {
+  expect(dropdown()).toBeTruthy();
+});
